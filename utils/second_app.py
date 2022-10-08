026319b48ab1c10bb9444a16f0b712a83d89b719
wangpengpeng <ppwang06@163.com>
2022-10-08 14:20:35 +0800

    [fixed] add new knowledge

--- utils/second_app.py
+++ utils/second_app.py
@@ -2,5 +2,9 @@
 猿人学 第二题
 输入参数: 1:1660532260
 输出：m4ToX3zKJr3BWdzQkBusCg==
-解决方式: 1. frida rpc 调用解决  2. unidbg 模拟运行  3. 算法还原(可能难度较大)
+解决方式:
+1. frida rpc 调用解决  [完成]
+2. unidbg 模拟运行
+3. 算法还原(可能难度较大)  [进行中]
+"kYYmZAxxt2nQP pcd28a37b28601f56089818f1074465cfb XDswPpZQiB" 16 + 16 + 32
 """
